fc1dd60cb90cc888cb17b214015890b3b62daf6a
Ilia Choly <ilia.choly@gmail.com>
2020-09-16 16:57:46 -0400

    hacking some stuff together

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/icholy/gomajor
 
 go 1.15
+
+require (
+	github.com/google/go-cmp v0.5.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sanity-io/litter v1.3.0
+	golang.org/x/mod v0.3.0
+	golang.org/x/tools v0.0.0-20200916195026-c9a70fc28ce3
+	gotest.tools v2.2.0+incompatible // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"path"
+	"strings"
+
+	"golang.org/x/mod/module"
+	"golang.org/x/mod/semver"
+	"golang.org/x/tools/go/packages"
+)
+
+func SplitSpec(spec string) (path, version string) {
+	parts := strings.SplitN(spec, "@", 2)
+	if len(parts) == 2 {
+		path = parts[0]
+		version = parts[1]
+	} else {
+		path = spec
+	}
+	return
+}
+
+func WithPathMajor(path, major string) string {
+	// remove the existing version if there is one
+	if prefix, _, ok := module.SplitPathVersion(path); ok {
+		path = prefix
+	}
+	if major == "v0" || major == "v1" || major == "" {
+		return path
+	}
+	if !strings.HasSuffix(path, "/") {
+		path += "/"
+	}
+	return path + major
+}
 
 func main() {
-	fmt.Println("vim-go")
+	flag.Parse()
+	if flag.NArg() != 1 {
+		log.Fatal("missing package spec")
+	}
+	// split the package package and version
+	spec := flag.Arg(0)
+	pkgpath, version := SplitSpec(spec)
+	// find the module root
+	cfg := &packages.Config{
+		Mode: packages.NeedName | packages.NeedModule,
+	}
+	pkgs, err := packages.Load(cfg, pkgpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pkg := pkgs[0]
+	// find the module path for the specified version
+	modpath := WithPathMajor(pkg.Module.Path, semver.Major(version))
+	fmt.Println(
+		path.Join(modpath, strings.TrimPrefix(pkg.PkgPath, pkg.Module.Path)),
+	)
 }
