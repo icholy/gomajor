8fdc41a8a654e837c5cf2176ee1d3494acdb9eb6
Ilia Choly <ilia.choly@gmail.com>
2022-06-20 13:48:49 -0400

    make the tempmod package more flexible

--- internal/tempmod/tempmod.go
+++ internal/tempmod/tempmod.go
@@ -2,47 +2,53 @@ package tempmod
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
 )
 
 type Mod struct {
-	Dir     string
-	Imports []string
+	Dir string
 }
 
-func (m *Mod) Delete() error {
+func Create(name string) (Mod, error) {
+	dir, err := os.MkdirTemp(os.TempDir(), "tempmod")
+	if err != nil {
+		return Mod{}, err
+	}
+	m := Mod{Dir: dir}
+	if name == "" {
+		name = "temp"
+	}
+	if err := m.ExecGo("mod", "init", name); err != nil {
+		return Mod{}, err
+	}
+	return m, nil
+}
+
+func (m Mod) ExecGo(args ...string) error {
+	cmd := exec.Command("go", args...)
+	cmd.Stderr = os.Stderr
+	cmd.Dir = m.Dir
+	return cmd.Run()
+}
+
+func (m Mod) Delete() error {
 	return os.RemoveAll(m.Dir)
 }
 
-func Init(imports []string) (*Mod, error) {
-	dir, err := os.MkdirTemp(os.TempDir(), "")
+func (m Mod) UsePackage(pkgpath string) error {
+	tmp, err := os.CreateTemp(m.Dir, "*-import.go")
 	if err != nil {
-		return nil, err
+		return err
 	}
-	cmd := exec.Command("go", "mod", "init", "tmp")
-	cmd.Dir = dir
-	if err := cmd.Run(); err != nil {
-		return nil, err
-	}
+	defer tmp.Close()
 	var src bytes.Buffer
 	src.WriteString("package tmp\n")
-	for _, path := range imports {
-		src.WriteString("import _ \"" + path + "\"\n")
-	}
+	fmt.Fprintf(&src, "import _ %q\n", pkgpath)
 	src.WriteString("func main() {}\n")
-	path := filepath.Join(dir, "main.go")
-	if err := os.WriteFile(path, src.Bytes(), os.ModePerm); err != nil {
-		return nil, err
+	if _, err := src.WriteTo(tmp); err != nil {
+		return err
 	}
-	cmd = exec.Command("go", "get")
-	cmd.Dir = dir
-	if err := cmd.Run(); err != nil {
-		return nil, err
-	}
-	return &Mod{
-		Dir:     dir,
-		Imports: imports,
-	}, nil
+	return tmp.Close()
 }
