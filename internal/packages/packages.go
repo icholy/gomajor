bf94a9165147ef4d4e71f93e505377e3ee61e5fa
Ilia Choly <ilia.choly@gmail.com>
2022-06-20 14:26:32 -0400

    packages: add LocalVersions

--- internal/packages/packages.go
+++ internal/packages/packages.go
@@ -2,7 +2,6 @@ package packages
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -119,7 +118,7 @@ func Direct(dir string) ([]module.Version, error) {
 	if err != nil {
 		return nil, err
 	}
-	data, err := ioutil.ReadFile(name)
+	data, err := os.ReadFile(name)
 	if err != nil {
 		return nil, err
 	}
@@ -130,6 +129,33 @@ func Direct(dir string) ([]module.Version, error) {
 	var mods []module.Version
 	for _, req := range file.Require {
 		if !req.Indirect {
+			mods = append(mods, req.Mod)
+		}
+	}
+	return mods, nil
+}
+
+// LocalVersions returns the locally required versions of modules with the provided prefix.
+func LocalVersions(dir, modprefix string) ([]module.Version, error) {
+	name, err := FindModFile(dir)
+	if err != nil {
+		return nil, err
+	}
+	data, err := os.ReadFile(name)
+	if err != nil {
+		return nil, err
+	}
+	file, err := modfile.ParseLax(name, data, nil)
+	if err != nil {
+		return nil, err
+	}
+	var mods []module.Version
+	for _, req := range file.Require {
+		if req.Indirect {
+			continue
+		}
+		modprefix0, _, ok := module.SplitPathVersion(req.Mod.Path)
+		if ok && modprefix == modprefix0 {
 			mods = append(mods, req.Mod)
 		}
 	}
