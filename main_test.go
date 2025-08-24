b2522b392967414e27292d09fdfc6b4f6b9bd7b0
Ilia Choly <ilia.choly@gmail.com>
2025-08-24 07:38:44 -0400

    Register main as gomajor command in testscript

--- main_test.go
+++ main_test.go
@@ -6,6 +6,12 @@ import (
 	"github.com/rogpeppe/go-internal/testscript"
 )
 
+func TestMain(m *testing.M) {
+	testscript.Main(m, map[string]func(){
+		"gomajor": main,
+	})
+}
+
 func TestHelpCommand(t *testing.T) {
 	testscript.Run(t, testscript.Params{
 		Dir: "testdata/testscript/help",
