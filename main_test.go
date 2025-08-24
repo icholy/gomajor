6a7e93de1a9b887b2ccc1b6a631cd91a6047cf3e
Ilia Choly <ilia.choly@gmail.com>
2025-08-24 10:30:08 -0400

    Add testscript for list command

--- main_test.go
+++ main_test.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"net/http/httptest"
 	"testing"
 
 	"github.com/rogpeppe/go-internal/testscript"
+
+	"github.com/icholy/gomajor/internal/modproxy/testmodproxy"
 )
 
 func TestMain(m *testing.M) {
 	testscript.Main(m, map[string]func(){
 		"gomajor": main,
+	})
+}
+
+func TestListCommand(t *testing.T) {
+	testscript.Run(t, testscript.Params{
+		Dir: "testdata/testscript/list",
+		Setup: func(env *testscript.Env) error {
+			proxy, err := testmodproxy.Load("testdata/modules")
+			if err != nil {
+				return err
+			}
+			server := httptest.NewServer(proxy)
+			env.Vars = append(env.Vars, "GOPROXY="+server.URL)
+			env.Defer(func() { server.Close() })
+			return nil
+		},
 	})
 }
 
